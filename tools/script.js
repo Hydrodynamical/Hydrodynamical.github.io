2f083ab209c88d90a42b14b20751295a8129a964
JKM <joseph.kent.miller@gmail.com>
2023-11-24 14:11:18 -0600

    modified:   tools/BibliographyTool.html 	modified:   tools/script.js

--- tools/script.js
+++ tools/script.js
@@ -9,3 +9,14 @@ function processInputs() {
     var result = `<li>${authors}. <span class = "italic">${title}</span>. ${journal}, ${year}. <a href="javascript:void(0);" onclick="toggleAbstract('${id}')">Abstract</a>.\n\t<div id="${id}" class="abstract">\n\t\t${abstractContents}\n\t</div>\n</li>`;
     document.getElementById('output').textContent = result;
 }
+
+function searchGoogle() {
+    var title = document.getElementById('title').value;
+    var authors = document.getElementById('authors').value;
+
+    // Construct the search query
+    var query = `${title} ${authors}`.trim().replace(/\s+/g, '+');
+
+    // Redirect to Google search
+    window.open(`https://www.google.com/search?q=${query}`, '_blank');
+}
